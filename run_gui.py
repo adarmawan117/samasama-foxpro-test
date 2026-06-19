575952018aabbd111003915bc2f384d3ba5f316b
adarmawan117 <adarmawan117@gmail.com>
2026-06-19 19:50:08 +0700

    trap gui

--- run_gui.py
+++ run_gui.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5.QtWidgets import QApplication
+import traceback
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from adjustment_ppn_gui import ProsesAdjustmentPajakApp, AdjustmentPajakController
+
+def global_exception_hook(exctype, value, tb):
+    err_msg = ''.join(traceback.format_exception(exctype, value, tb))
+    print(err_msg, file=sys.stderr)
+    try:
+        with open("crash_log.txt", "w") as f:
+            f.write(err_msg)
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Critical)
+        msg_box.setWindowTitle("Fatal Error")
+        msg_box.setText("Aplikasi berhenti secara tidak terduga!")
+        msg_box.setDetailedText(err_msg)
+        msg_box.exec_()
+    except:
+        pass
+    sys.exit(1)
+
+sys.excepthook = global_exception_hook
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
